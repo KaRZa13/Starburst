680104ad93d8cb89a6dab844399e23be11405cca
Gashrod1 <101563672+Gashrod1@users.noreply.github.com>
2024-02-12 18:17:24 +0100

    Début d'implémentation boss

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import pygame
 from os import path
 from player import Player
 from game import Game
+from boss import Boss
+
+
+def render():
+    screen.blit(background, (background_rel_x - background_rect.width, 0))
+    screen.blit(background, (background_rel_x, 0))
+    all_sprites.draw(screen)
+    player.update_bar(screen)
+    player.create_flame(all_sprites)
+    game.draw_text(f"Score : {player.score}", 20, 10, 10, screen)
+    game.draw_text(f"Health : {player.health}", 20, 10, 40, screen)
+    game.draw_text(f"Level : {level}", 20, 10, 70, screen)
+
 
 # Variables de dossier assets
 img_dir_player = path.join(path.dirname(__file__), "assets/img/player")
@@ -61,12 +74,12 @@ background_rect = background.get_rect()
 
 all_sprites = pygame.sprite.Group()
 player_projectiles = pygame.sprite.Group()
+boss_projectiles = pygame.sprite.Group()
 bumpers = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
 player_flames = pygame.sprite.Group()
 
 player = Player(screen, all_sprites, player_projectiles, player_flames, shoot_sound)
-
 
 # Variable de jeu
 running = True
@@ -79,13 +92,15 @@ next_level = 100
 background_x = 0
 bumper_loop = 0
 base_velocity = 3
+boss_level = 10
+boss_spawned = False
 velocity = base_velocity
 
 
 while running:
 
     if title_screen:
-        screen.fill((0, 0, 0))  # Remplir l'écran avec une couleur de fond
+        screen.fill((0, 0, 0))
         game.draw_text("Starburst", 64, WIDTH / 2, HEIGHT / 2 - 100, screen)
         game.draw_text("Press any key to start", 36, WIDTH / 2, HEIGHT / 2, screen)
         pygame.display.flip()
@@ -102,7 +117,7 @@ while running:
                     title_screen = False
 
     if game_over:
-        screen.fill((0, 0, 0))  # Remplir l'écran avec une couleur de fond
+        screen.fill((0, 0, 0))
         game.draw_text("Game Over", 64, WIDTH / 2, HEIGHT / 2, screen)
         game.draw_text(
             "Press any key to go to title", 36, WIDTH / 2, HEIGHT / 2 + 50, screen
@@ -140,11 +155,14 @@ while running:
         all_sprites.add(player)
 
     # Création des ennemies
-    if len(enemies) == 0:
-        min_enemy = player.power * 2
-        max_enemy = player.power * 5
-        nb = random.randrange(min_enemy, max_enemy)
-        game.spawn_enemy(nb, enemies_img, player, all_sprites, enemies, WIDTH, HEIGHT)
+    if not boss_spawned:
+        if len(enemies) == 0:
+            min_enemy = player.power * 2
+            max_enemy = player.power * 5
+            nb = random.randrange(min_enemy, max_enemy)
+            game.spawn_enemy(
+                nb, enemies_img, player, all_sprites, enemies, WIDTH, HEIGHT
+            )
 
     # Gestion des évènements (clavier)
     for event in pygame.event.get():
@@ -179,31 +197,28 @@ while running:
         pygame.sprite.Group(player), bumpers, False, True
     )
 
-    # Gestion des collisions avec les ennemis
+    # Gestion des collisions des projectiles avec les ennemis
     for projectile, enemy_list in collisions.items():
         for enemy in enemy_list:
-            # Réduction de la santé de l'ennemi, augmentation du score, etc.
             enemy.health -= projectile.damage
             player.score += enemy.points
 
-    # Gestion des collisions avec les "bumper"
+    # Gestion des collisions des projectiles avec les bumper
     for projectile, bumper_list in collisions_with_bumpers.items():
         for bumper in bumper_list:
-            # Réduction de la santé du bumper, augmentation du score, etc.
             bumper.health -= projectile.damage
             player.score += bumper.points
 
+    # Gestion des collisions du joueur avec les bumpers
     for player, bumper_list in player_collisions_with_bumpers.items():
         for bumper in bumper_list:
-            # Réduction de la santé du joueur et du bumper, augmentation du score, etc.
             player.health -= bumper.body_damage
             bumper.health -= player.body_damage
             player.score += bumper.points
 
-    # Gestion des collisions avec les ennemis
+    # Gestion des collisions du joueur avec les ennemis
     for player, enemy_list in player_collisions.items():
         for enemy in enemy_list:
-            # Réduction de la santé du joueur et de l'ennemi, augmentation du score, etc.
             player.health -= enemy.body_damage
             enemy.health -= player.body_damage
             player.score += enemy.points
@@ -212,6 +227,7 @@ while running:
     if player.health <= 0:
         game_over = True
 
+    # Gestion des niveaux
     if player.score >= next_level:
         level += 1
         next_level *= 3
@@ -220,11 +236,12 @@ while running:
             bumper.velocity = velocity
 
     # Boucle pour créer les bumpers
-    bumper_loop += dt
-    if bumper_loop >= 250:  # Vous pouvez ajuster cette condition selon vos besoins
-        game.spawn_bumper(all_sprites, bumpers, bumper_anim, velocity)
-        bumper_loop = 0
+    if not boss_spawned:
         bumper_loop += dt
+        if bumper_loop >= 250:
+            game.spawn_bumper(all_sprites, bumpers, bumper_anim, velocity)
+            bumper_loop = 0
+            bumper_loop += dt
 
     # Mise à jour des sprites
     all_sprites.update()
@@ -235,14 +252,12 @@ while running:
     background_rel_x = background_x % background_rect.width
 
     # Draw / Render
-    screen.blit(background, (background_rel_x - background_rect.width, 0))
-    screen.blit(background, (background_rel_x, 0))
-    all_sprites.draw(screen)
-    player.update_bar(screen)
-    player.create_flame(all_sprites)
-    game.draw_text(f"Score : {player.score}", 20, 10, 10, screen)
-    game.draw_text(f"Health : {player.health}", 20, 10, 40, screen)
-    game.draw_text(f"Level : {level}", 20, 10, 70, screen)
+    render()
+
+    """if level == boss_level:
+        boss_spawned = True
+        boss = Boss(screen, all_sprites, boss_projectiles, shoot_sound, player)
+        all_sprites.add(boss)"""
 
     pygame.display.flip()
 
